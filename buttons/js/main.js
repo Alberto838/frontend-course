3ee25f6731b42ef41e80583d15fe4ab333e02ef2
Alberto838 <albert34321@gmail.com>
2022-06-16 00:02:19 +0200

    added buttons

---
+++ buttons/js/main.js
@@ -0,0 +1,18 @@
+let tab = [1, 2, 3, 4, 5, 6]
+
+for (let i = 0; i < tab.length; i++) {
+	console.log(tab[i])
+}
+
+function fun(arg1, arg2, arg3) {
+	console.log(arg1 + arg2 * arg3)
+}
+
+fun(1, 2, 3)
+
+if (tab.length > 5) {
+	console.log('TRUE')
+} else {
+	console.log('FALSE')
+}
+ 
