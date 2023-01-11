8c9239934c2fe8bdcc19de5aee84c5b6c19927b0
Alberto838 <albert34321@gmail.com>
2023-01-12 00:29:18 +0100

    added javascript folder

---
+++ javascript/js/main.js
@@ -0,0 +1,9 @@
+const name = 'Albert'
+let age = 22
+const favDish = 'pasta e pizza'
+
+console.log(name)
+console.log(age)
+console.log(favDish)
+
+console.log('Mi chiamo ' + name + ', ho ' + age + ' anni' + ', il mio piatto preferito è: ' + favDish + '.')
