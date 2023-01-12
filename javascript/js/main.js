624bdd4cb7175a3e5888a812f6bced1c34485264
Alberto838 <albert34321@gmail.com>
2023-01-12 23:18:03 +0100

    done some exercises

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,9 +1,50 @@
+/*
 const name = 'Albert'
+const animalName = 'Kulka'
 let age = 22
+let animalAge = 2
 const favDish = 'pasta e pizza'
 
 console.log(name)
 console.log(age)
 console.log(favDish)
 
-console.log('Mi chiamo ' + name + ', ho ' + age + ' anni' + ', il mio piatto preferito è: ' + favDish + '.')
+console.log('Mi chiamo ' + name + ', ho ' + age + ' anni, il mio piatto preferito è: ' + favDish + '.')
+console.log(`Mi chiamo ${name}, ho ${age} anni, il mio piatto preferito è: ${favDish}.`)
+console.log(`Ciao, sono ${name}, e questa è ${animalName} - il mio cane.`);
+console.log(animalName + ' ha già ' + animalAge + ' anni!');
+*/
+
+/*
+BEFORE:
+
+const firstName = 'Tomek'
+let age = 12
+age = 18
+ 
+var favColor = 'niebieski'
+const fav-meal = 'schabowy'
+ 
+let current.Car
+current.Car = 'Audi'
+ 
+let firstName = 'Ania'
+let age2 = 24
+let ulubionyKolor = 'czerwony'
+*/
+
+// AFTER:
+
+const firstName = 'Tomek'
+let age = 12
+age = 18
+ 
+const favColor = 'niebieski'
+const favMeal = 'schabowy'
+ 
+let currentCar
+currentCar = 'Audi'
+ 
+let firstName2 = 'Ania'
+let age2 = 24
+let favColor2 = 'czerwony'
