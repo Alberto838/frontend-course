5774b658e4c67cfb69866ce34e01e2a488f8f2b2
Alberto838 <albert34321@gmail.com>
2023-01-15 18:26:53 +0100

    done exercise

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -35,16 +35,34 @@ let ulubionyKolor = 'czerwony'
 
 // AFTER:
 
-const firstName = 'Tomek'
-let age = 12
-age = 18
+// const firstName = 'Tomek'
+// let age = 12
+// age = 18
  
-const favColor = 'niebieski'
-const favMeal = 'schabowy'
+// const favColor = 'niebieski'
+// const favMeal = 'schabowy'
  
-let currentCar
-currentCar = 'Audi'
+// let currentCar
+// currentCar = 'Audi'
  
-let firstName2 = 'Ania'
-let age2 = 24
-let favColor2 = 'czerwony'
+// let firstName2 = 'Ania'
+// let age2 = 24
+// let favColor2 = 'czerwony'
+
+// -------------- EXERCISE
+
+const text1 = 'powiększ mnie!'
+const text2 = 'ZAPISZ MNIE MAŁYMI LITERAMI'
+const text3 = '$#%#^ wytnij te dziwne znaki na początku!'
+const text4 = 'sprawdź, czy zawieram słowo "czy"'
+const text5 = 'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"'
+const text6 = 'pies zamień każde słowo pies, na słowo kot pies'
+const text7 = 'podziel, ten, string, od, przecinków'
+
+console.log(text1.toUpperCase());
+console.log(text2.toLowerCase());
+console.log(text3.slice(6));
+console.log(text4.includes('czy'));
+console.log(text5.charAt(2));
+console.log(text6.replaceAll('pies', 'kot'));
+console.log(text7.split(','));
